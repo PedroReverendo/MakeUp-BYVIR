688ca41c799a10b55e932f009b41be4721ca112b
Pedro Reverendo <pedroreverendo04@gmail.com>
2021-09-08 00:24:41 -0300

    hola

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+  }
+  showMenu('nav-toggle','nav-menu')
+  
+  /*==================== REMOVE MENU MOBILE ====================*/
+  const navLink = document.querySelectorAll('.nav__link')
+  
+  function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+  }
+  navLink.forEach(n => n.addEventListener('click', linkAction))
+  
+/*==================== SWIPER DISCOVER ====================*/
+let swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
+
+/*=============== ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.accordion__item')
+
+// 1. Selecionar cada item
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.accordion__header')
+
+    // 2. Seleccionar cada click del header
+    accordionHeader.addEventListener('click', () =>{
+        // 7. Crear la variable
+        const openItem = document.querySelector('.accordion-open')
+        
+        // 5. Llamar a la funcion toggle item
+        toggleItem(item)
+
+        // 8. Validar si existe la clase
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+// 3. Crear una funcion tipo constante
+const toggleItem = (item) =>{
+    // 3.1 Crear la variable
+    const accordionContent = item.querySelector('.accordion__content')
+
+    // 6. Si existe otro elemento que contenga la clase accorion-open que remueva su clase
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        // 4. Agregar el height maximo del content
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
